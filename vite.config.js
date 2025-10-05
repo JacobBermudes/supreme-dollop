d19f641beb233451c1bd3867ee89cd51d3a7560b
erokhinms <erokhinms@bcs.ru>
2025-10-05 19:37:25 +0500

    build by pipe

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import fs from 'fs';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/supreme-dollop/'
-});
+export default {
+    base: '/supreme-dollop/'
+}
