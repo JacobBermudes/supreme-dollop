9a81a5e287ad112e3fbcff16171ae49825cdd0d3
erokhinms <erokhinms@bcs.ru>
2025-10-05 19:26:04 +0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,5 @@ import fs from 'fs';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: 'myvpn12.com',
-    https: {
-      key: fs.readFileSync('./myvpn12.com-key.pem'),
-      cert: fs.readFileSync('./myvpn12.com.pem'),
-    },
-  },
+  base: '/supreme-dollop/'
 });
