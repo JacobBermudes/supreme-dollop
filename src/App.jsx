afc27831d891d8f371aa8ed478cc9b3bf40b5e07
erokhinms <erokhinms@bcs.ru>
2025-11-07 09:56:31 +0500

    Tariff, balance, username webview

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,42 @@ import { useEffect, useState } from 'react';
 
 export default function App() {
   const [user, setUser] = useState(null);
+  const [accData, setAccData] = useState(null)
 
   useEffect(() => {
     if (window.Telegram?.WebApp) {
       window.Telegram.WebApp.ready();
       const initData = window.Telegram.WebApp.initDataUnsafe;
+
+      const payload = {
+        uid: initData.user.id,
+        username: initData.user.username
+      };
+
       setUser(initData.user || { id: 'Неизвестно', username: 'Гость' });
+
+      const sendReq = async () =>{
+      try {
+        const resp = await fetch('phunkao.fun/api', {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify(payload)
+        });
+
+        if (!resp.ok) {
+          throw new Error(`HTTP Error. ${resp.status}: ${resp.statusText}`);
+        }
+
+        const tg_data = await resp.json();
+        setAccData(tg_data)
+      } catch (err) {
+          throw new Error(`HTTP Error. ${err.message}`);
+      } 
+    };
+
+    sendReq()
     }
   }, []);
 
@@ -24,14 +54,14 @@ export default function App() {
   return (
     <div className="card">
       <h2>Привет!</h2>
-      <p>Твой Telegram ID:</p>
-      <div className="id" onClick={() => copyToClipboard(user.id)}>
-        {user.id}
+      <div className="id" onClick={() => copyToClipboard(accData.username || '—')}>
+        @{accData.username || 'не указан'}
       </div>
-
-      <p>Твой username:</p>
-      <div className="id" onClick={() => copyToClipboard(user.username || '—')}>
-        @{user.username || 'не указан'}
+      <div className="id" onClick={() => copyToClipboard(accData.balance || '—')}>
+        @{accData.balance || 'Баланс неизвестен'}
+      </div>
+      <div className="id" onClick={() => copyToClipboard(accData.tariff || '—')}>
+        @{accData.tariff || 'Тариф неизвестен'}
       </div>
 
       <button onClick={() => window.Telegram.WebApp.close()}>
